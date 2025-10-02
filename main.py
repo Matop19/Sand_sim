0b5bc3c533b78a9e0ebb4953ed792d5fd34986d2
Matop19 <m.mespoulet@gmail.com>
2025-10-02 23:53:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+from Variables import *
 import particlesManager
+import particles
 import pygame
 pygame.init()
+#Différents imports + initiation de pygame
+
 def window():
-    width, height = 1400, 800
     screen = pygame.display.set_mode((width, height))
-    blocksize = 10
     pygame.display.set_caption("Sand_simulation")
     clock = pygame.time.Clock()
+    #initier la page
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+    #permet de quitter la fenêtre
+
         screen.fill((225,225,225))
         for row in range(0,width):
             for col in range(0,height):
@@ -19,10 +25,19 @@ def window():
                                    row * blocksize,
                                    blocksize, blocksize)
                 pygame.draw.rect(screen, (0,0,0), rect, 1)
-        picker = pygame.Rect(width-200, 0, 200, height)
-        pygame.draw.rect(screen, (0,0,0), picker, 0)
+        #Création de la grille
+
+        palette = pygame.Rect(width-200, 0, 200, height)
+        pygame.draw.rect(screen, (0,0,0), palette, 0)
+        pygame.draw.circle(screen, particles.Water.color, (particles.Water.x,particles.Water.y), circle_radius)
+        pygame.draw.circle(screen, particles.Sand.color, (particles.Sand.x, particles.Sand.y), circle_radius)
+        pygame.draw.circle(screen, particles.Lava.color, (particles.Lava.x, particles.Lava.y), circle_radius)
+        pygame.draw.circle(screen, particles.Rock.color, (particles.Rock.x, particles.Rock.y), circle_radius)
+        pygame.draw.circle(screen, particles.Steel.color, (particles.Steel.x, particles.Steel.y), circle_radius)
+        pygame.draw.circle(screen, particles.Glass.color, (particles.Glass.x, particles.Glass.y), circle_radius)
+        #choisir la matière
+
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
 window()
-
