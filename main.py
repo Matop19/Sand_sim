91946740702f7d41b176418c6c9f88946e1f312e
Matop19 <m.mespoulet@gmail.com>
2025-10-02 09:56:27 +0200

    En train de faire la fenêtre
    Je continue dans 15 minutes

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import particlesManager
 import pygame
+pygame.init()
+def window():
+  width, height = 1400, 800
+  screen = pygame.display.set_mode((width, height))
+  blocksize = 2
+  pygame.display.set_caption("Sand_simulation")
+  clock = pygame.time.Clock()
+  running = True
+  while running:
+      for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              running = False
