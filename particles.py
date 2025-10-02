576c350f056321d5a0f10b267117dc3e13747e89
Sachuniku <sachunikuothers@gmail.com>
2025-10-02 09:54:33 +0200

    Update particles.py

--- particles.py
+++ particles.py
@@ -1,3 +1,14 @@
+class Empty():
+	index = 0
+	rules = []
+	color = (255,255,255)
+
 class Sand():
-	rule = ""
+	index = 1
+	rules = ["1FFFF0FFF100001000"]
 	color = (237,237,26)
+
+class Rock():
+	index = 2
+	rules = ["2FFFF0FFF200001000"]
+	color = (60,60,60)
